9c8fc59c3cf199c4bfe80cc9b9d6fd885f2917c5
Rahul Sharma <hubstackrealm@gmail.com>
2026-08-21 14:00:42 +0530

    Clean up SQL dump by removing extraneous lines
    Removed unnecessary SQL settings and comments.

--- part-2/library_management (1).sql
+++ part-2/library_management (1).sql
@@ -1,23 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
 -- Generation Time: Aug 21, 2026 at 08:29 AM
--- Server version: 8.4.7
 -- PHP Version: 8.3.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `library_management`
 --
 
@@ -48,6 +32,3 @@ INSERT INTO `books` (`isbn`, `author_name`, `book_title`, `publication_date`) VA
 (5, 'FRANK HERBERT', 'DUNE', '0000');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

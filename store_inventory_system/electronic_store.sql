35a5c43a0bede207c0fb68160d620babbab1509e
Rahul Sharma <hubstackrealm@gmail.com>
2026-08-24 13:55:06 +0530

    Clean up SQL dump by removing extraneous settings
    Removed unnecessary SQL settings and transaction commands.

--- store_inventory_system/electronic_store.sql
+++ store_inventory_system/electronic_store.sql
@@ -1,21 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
 -- Generation Time: Aug 24, 2026 at 08:23 AM
--- Server version: 8.4.7
 -- PHP Version: 8.3.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `electronic_store`
@@ -53,6 +39,3 @@ INSERT INTO `products` (`product_id`, `product_name`, `brand_name`, `product_cat
 (5, 'MX MASTER3S', 'LOGITECH', 'MOUSE', 8999.99, '2025', 16, 'LOG-MX3S-7N56');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
